dd4abab04e0262513a7da655636c3f623b06a580
DevAlbert97 <dev.carloshudm@gmail.com>
2022-09-12 13:37:53 -0700

    Forms by template dinamics

--- src/app/template/dinamics/dinamics.component.ts
+++ src/app/template/dinamics/dinamics.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Person {
+  name      : string;
+  favorits  : Favorit[];
+}
+
+interface Favorit {
+  id    : number;
+  name  : string;
+}
 
 @Component({
   selector: 'app-dinamics',
   templateUrl: './dinamics.component.html',
   styleUrls: ['./dinamics.component.css']
 })
-export class DinamicsComponent implements OnInit {
+export class DinamicsComponent {
 
-  constructor() { }
+  person: Person = {
+    name: 'Carlos',
+    favorits: [
+      {id: 1, name: 'Metal Gear'},
+      {id: 2, name: 'Death Stranding'},
+      
+    ]
+  }
 
-  ngOnInit(): void {
+  newGame: string = '';
+
+  save() {
+    console.log("Formulario guardado")
+  }
+
+  delete(index: number) {
+    this.person.favorits.splice(index,1);
+  }
+
+  addGame() {
+    const newFavorit: Favorit = {
+      id: this.person.favorits.length+1,
+      name: this.newGame
+    }
+    this.person.favorits.push({...newFavorit});
+    this.newGame="";
   }
 
 }
